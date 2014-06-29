29d19d15770c9babeeef1ad14902d5c9c80c980b
braun <braun@infinity.local>
2014-06-29 00:10:06 -0400

    added source

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-var app = angular.module('app',[]);
+angular
+	.module('app',['wu.masonry'])
+	.factory('MasonFactory',function MasonFactory() {
+		var mason = function () {
+			this.stones = [new Stone({
+				header: "This is the header.",
+				body: "This is the body, and it is a nice one."
+			}),new Stone({
+				header: "This is the header.",
+				body: "This is the body, and it is a nice one."
+			})];
+		}
+		mason.prototype.mortar = function (mortar) {
+			var wall = angular.element('#wall');
+			wall.masonry(mortar);
+		}
+		return mason;
+		function Stone (stone) {
+			this.header = stone.header;
+			this.body = stone.body;
+		}
+	})
+	.controller('AppController',['$scope','MasonFactory',function AppController($scope,MasonFactory) {
+		$scope.mason = new MasonFactory();
+		$scope.mason.mortar({
+	        columnWidth:  300,
+	        itemSelector: '.brick',
+	        isFitWidth: true
+	    });
+	}])
+;
